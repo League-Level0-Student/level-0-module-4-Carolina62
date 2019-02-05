47f3c780b3e7aee41e599e2de86dc35aa9efefe9
61653 <61653@0338S-S-R0B6ZHB.sdcs.local>
2019-02-05 14:24:45 -0800

    ChangeCalculator TestScores

---
+++ src/TestScores.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class TestScores {
+public static void main(String[] args) {
+	JOptionPane.showInternalMessageDialog(null, "What was your test score?");
+}
+
+}
