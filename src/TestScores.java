e5bf41544f90f8e4d0bfdd386f05cf1f0569bd8f
61653 <61653@0338S-S-R0B6ZHB.sdcs.local>
2019-02-12 14:21:57 -0800

    TestScores

--- src/TestScores.java
+++ src/TestScores.java
@@ -2,7 +2,21 @@ import javax.swing.JOptionPane;
 
 public class TestScores {
 public static void main(String[] args) {
-	JOptionPane.showInternalMessageDialog(null, "What was your test score?");
+	String test = JOptionPane.showInputDialog("What was your test score?");
+	double scores = Double.parseDouble(test);
+	if (scores>=80) {
+		JOptionPane.showMessageDialog(null, "Great Job, You must have studied really hard");
+		
+	}else if(scores>=70) {
+		JOptionPane.showMessageDialog(null, "Good Job, You Passed");
+		
+	}else if(scores<=69.9) {
+		JOptionPane.showMessageDialog(null, "Study harder to get a better score next time");
+	}
+			
+
+	
+	
 }
 
 }
